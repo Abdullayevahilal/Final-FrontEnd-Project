77db213882c282db9bebff38fc808cccca689ffc
abdullayevahilal <HilalMa@code.edu.az>
2019-12-13 23:45:28 +0400

    Our Client Finished

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,19 +1,37 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     //Features//
 
-    $(".feature-body").mouseenter(function(){
+    $(".feature-body").mouseenter(function () {
         $(this).find(".round").removeClass("d-none").addClass("d-block")
 
     })
-    $(".feature-body").mouseleave(function(){
+    $(".feature-body").mouseleave(function () {
         $(this).find(".round").removeClass("d-block").addClass("d-none")
     });
     $("#service-slider").owlCarousel();
+
+
+    //Client slider
+
+    $('.regular').slick({
+        arrows: false,
+        slidesToShow: 6,
+        slidesToScroll: 1
+    });
+
+    $(window).scroll(function(){
+        a = $(window).scrollTop()
+        if (a>100){
+            $('.navbar').addClass('fixed-top')
+        }else{
+            $('.navbar').removeClass('fixed-top')
+        }
+    })
 })
 //About Us//
-$(".slider").slick({
-    infinity:true,
-    slidesToShow:6,
-    slidesToScroll:1
-})
+// $(".slider").slick({
+//     infinity:true,
+//     slidesToShow:6,
+//     slidesToScroll:1
+// })
